566bf29d3c8a683286db1504652cbcb5d13475dc
Nils Urbach <ndu01u@gmail.com>
2023-12-04 15:04:30 +0100

    pls run

--- Day01/partone.c
+++ Day01/partone.c
@@ -1,3 +1,7 @@
-static void main(int argc, char *argv[]) {
-    
+#include <stdio.h>
+#include "..\lib\std\fileio.h"
+
+int main(int argc, char *argv[]) {
+    char *content = read_file("C:\\Users\\Nils\\Desktop\\AoC-2023\\Day01\\example.txt");
+    return 0;
 }
